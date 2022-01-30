207749d381dee0876a355f8b199bcdfb331e0e94
tobire <mail@tobias-reinke.de>
2022-01-30 18:54:00 +0100

    Initial commit with basic index page and room page

---
+++ src/routes/[roomId].svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	import Fa from 'svelte-fa';
+	import { faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
+
+	let roomId: string = $page['params']['roomId'];
+
+	let ws: WebSocket;
+
+	let shotclock: number;
+	let shotclockString: string = '';
+	$: if (shotclock !== undefined) shotclockString = shotclock.toString().padStart(2, '0');
+	let shotclockRed: boolean = false;
+
+	let alarmTimer: ReturnType<typeof setInterval>;
+	let audioActive: boolean = false;
+	let audio: HTMLAudioElement;
+
+	onMount(() => {
+		join();
+		audio = new Audio('audio/beep.mp3');
+	});
+
+	function activateAudio() {
+		audioActive = true;
+		audio.play();
+		audio.pause();
+		audio.load();
+	}
+
+	function join() {
+		ws = new WebSocket('wss://tobias-reinke.de/shotclock/' + roomId);
+		ws.onerror = function (error) {
+			console.log(error);
+		};
+		ws.onmessage = function (event) {
+			let id: string = event.data.substring(0, 1);
+			let data: Array<string> = event.data.substring(2).includes(';')
+				? event.data.substring(2).split(';')
+				: [event.data.substring(2)];
+			switch (id) {
+				case 't':
+					shotclock = parseInt(data[1]);
+					if (shotclock == 0) alarm();
+					else deactivateAlarm();
+					break;
+			}
+		};
+
+		function alarm() {
+			if (alarmTimer === undefined) {
+				shotclockRed = true;
+				beep();
+				alarmTimer = setInterval(function () {
+					shotclockRed = !shotclockRed;
+				}, 500);
+			}
+		}
+
+		function deactivateAlarm() {
+			if (alarmTimer) {
+				clearInterval(alarmTimer);
+				alarmTimer = undefined;
+				shotclockRed = false;
+			}
+		}
+
+		function beep() {
+			if (audioActive) {
+				audio.play();
+				setTimeout(function () {
+					audio.pause();
+					audio.load();
+				}, 2000);
+			}
+		}
+	}
+</script>
+
+<div id="main" class="w-screen h-screen overflow-hidden" on:click|once={activateAudio}>
+	<div class="absolute top-3 left-3" on:click={() => (audioActive = !audioActive)}>
+		{#if audioActive}
+			<Fa icon={faVolumeUp} size="2x" />
+		{:else}
+			<Fa icon={faVolumeMute} size="2x" />
+		{/if}
+	</div>
+	<div class="h-full w-full flex flex-col justify-center items-center">
+		<span class="clock" class:text-red-600={shotclockRed}>{shotclockString}</span>
+	</div>
+</div>
+
+<style lang="postcss">
+  #main {
+    @apply bg-black text-white;
+  }
+
+  .clock {
+    font-family: 'SevenSeg';
+    font-size: 65vmin;
+  }
+
+	@font-face {
+		font-family: 'SevenSeg';
+		font-style: normal;
+		src: url('fonts/DSEG7Classic-Regular.ttf') format('ttf'),
+			url('fonts/DSEG7Classic-Regular.woff2') format('woff2'),
+			url('fonts/DSEG7Classic-Regular.woff') format('woff');
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let roomIdJoin: string;
+	let roomIdCreate: string = generateRoomId();
+
+	function generateRoomId(): string {
+		const min: number = 10000;
+		const max: number = 99999;
+		return Math.floor(Math.random() * (max - min + 1) + min).toString();
+	}
+
+	function joinRoom() {
+		if (roomIdJoin) goto('/' + roomIdJoin);
+	}
+</script>
+
+<div id="main" class="w-screen h-screen">
+	<div class="flex justify-center h-full p-10">
+		<div class="flex flex-col justify-between h-full">
+			<div class="flex flex-col items-center">
+				<div class="flex flex-col items-center">
+					<div class="flex flex-col items-center mb-5">
+						<h1 class="text-white text-5xl mb-2">DLaxV Shot Clock</h1>
+						<h3 class="text-white text-xl">Free & Open Source online shot clock application</h3>
+					</div>
+					<div class="flex mb-3 justify-between">
+						<input class="w-8/12 px-3 py-2 rounded" placeholder="Room ID" bind:value={roomIdJoin} />
+						<button class="btn" on:click={joinRoom}>Join Room</button>
+					</div>
+				</div>
+				<div class="flex items-center">
+					<button class="btn">Instructions</button>
+					<button class="btn">Create Room</button>
+				</div>
+			</div>
+
+			<!-- Logos -->
+			<div class="h-40 flex flex-col items-center justify-between">
+				<div class="text-white font-bold text-2xl">Supported by</div>
+				<div class="flex h-4/6">
+					<div class="h-full mx-5">
+						<a href="https://dlaxv.de/" target="_blank">
+							<img src="img/logo-dlaxv.svg" alt="Logo DLaxV" class="h-full" />
+						</a>
+					</div>
+					<div class="h-full mx-5">
+						<a href="https://worldlacrosse.sport/" target="_blank">
+							<img src="img/logo-worldlacrosse.svg" alt="Logo World Lacrosse" class="h-full" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	#main {
+		background: rgb(158, 158, 158);
+		background: radial-gradient(
+			circle,
+			rgba(158, 158, 158, 1) 0%,
+			rgba(117, 117, 117, 1) 35%,
+			rgba(66, 66, 66, 1) 100%
+		);
+	}
+
+	.btn {
+		@apply p-2 mx-2 border rounded text-white text-xl;
+	}
+</style>
