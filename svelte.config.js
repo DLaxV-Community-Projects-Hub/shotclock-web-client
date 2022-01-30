0103b161141dd5dce1d2cba0a99a0eb1b160b175
tobire <mail@tobias-reinke.de>
2022-01-30 19:07:24 +0100

    Added configuration for gh-pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import static_adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
+
+console.log('Dev build: ' + dev);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: static_adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    paths: {
+      base: dev ? '' : '/shotclock-web-client',
+    },
+  }
 };
 
 export default config;
