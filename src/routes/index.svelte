7d837d08e9af770613ea3ce01d185b36aecf9297
tobire <mail@tobias-reinke.de>
2022-01-30 21:33:59 +0100

    Added create room layout on index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,14 +5,15 @@
 
 	let loaded: boolean = false;
 
-	let roomIdJoin: string;
-	let roomIdCreate: string = generateRoomId();
+	let joinId: string;
+	let createId: string = generateRoomId();
+	let createPin: string;
 
 	onMount(() => {
 		// Redirect traffic from 404 page to corresponding room
 		let path: string = new URL(window.location.href).pathname;
 		if (path !== base + '/') {
-			roomIdJoin = new URL(window.location.href).pathname.replace(base + '/', '');
+			joinId = new URL(window.location.href).pathname.replace(base + '/', '');
 			joinRoom();
 		} else loaded = true;
 	});
@@ -24,37 +25,53 @@
 	}
 
 	function joinRoom() {
-		if (roomIdJoin) goto(base + '/' + roomIdJoin);
+		if (joinId) goto(base + '/' + joinId);
 	}
+
+	function createRoom() {}
 </script>
 
 {#if loaded}
-	<div id="main" class="w-screen h-screen">
-		<div class="flex justify-center h-full p-10">
-			<div class="flex flex-col justify-between h-full">
-				<div class="flex flex-col items-center">
-					<div class="flex flex-col items-center">
-						<div class="flex flex-col items-center mb-5">
-							<h1 class="text-white text-5xl mb-2">DLaxV Shot Clock</h1>
-							<h3 class="text-white text-xl">Free & Open Source online shot clock application</h3>
+	<div id="main" class="w-screen h-screen flex justify-center">
+		<div class="flex justify-center p-5 lg:p-10 h-full w-full lg:w-1/2">
+			<div class="flex flex-col justify-between h-full w-full">
+				<div class="flex flex-col items-center w-full">
+					<div class="flex flex-col items-center w-full">
+						<div class="flex flex-col items-center mb-5 lg:mb-10">
+							<h1 class="text-white text-3xl md:text-4xl lg:text-5xl mb-2">DLaxV Shot Clock</h1>
+							<h3 class="text-white text-md lg:text-xl text-center">
+								Free & Open Source online shot clock application
+							</h3>
 						</div>
-						<div class="flex mb-3 justify-between">
-							<input
-								class="w-8/12 px-3 py-2 rounded"
-								placeholder="Room ID"
-								bind:value={roomIdJoin}
-							/>
-							<button class="btn" on:click={joinRoom}>Join Room</button>
+						<div class="w-full lg:w-7/12 mb-10 lg:mb-20">
+							<div class="mb-3">
+								<h4 class="text-white text-md uppercase">Join room</h4>
+							</div>
+							<div class="grid grid-cols-inputs-2 mb-3">
+								<span class="uppercase text-white text-xs font-bold">Room ID</span>
+								<div />
+								<input class="px-3 py-2 rounded" bind:value={joinId} />
+								<button class="btn" on:click={joinRoom}>Join</button>
+							</div>
 						</div>
-					</div>
-					<div class="flex items-center">
-						<button class="btn">Instructions</button>
-						<button class="btn">Create Room</button>
+						<div class="w-full lg:w-7/12">
+							<div class="mb-3">
+								<h4 class="text-white text-md uppercase">Create room</h4>
+							</div>
+							<div class="grid grid-cols-inputs-3 mb-3">
+								<span class="uppercase text-white text-xs font-bold">Room ID</span>
+								<span class="uppercase text-white text-xs font-bold">Control PIN</span>
+								<div />
+								<input class="px-3 py-2 mr-2 rounded" bind:value={createId} />
+								<input class="px-3 py-2 rounded" bind:value={createPin} />
+								<button class="btn" on:click={createRoom}>Create</button>
+							</div>
+						</div>
 					</div>
 				</div>
 
 				<!-- Logos -->
-				<div class="h-40 flex flex-col items-center justify-between">
+				<div class="h-32 lg:h-40 flex flex-col items-center justify-between">
 					<div class="text-white font-bold text-2xl">Supported by</div>
 					<div class="flex h-4/6">
 						<div class="h-full mx-5">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'inputs-2': 'minmax(0, 1fr) auto',
+        'inputs-3': 'repeat(2, minmax(0, 1fr)) auto'
+      }
+    },
   },
   plugins: [],
 }
