72c80c37cb0a2eb866ab30b7d225029e640192c4
tobire <mail@tobias-reinke.de>
2022-01-31 15:32:19 +0100

    Create room settings (with pin)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,8 +6,10 @@
 	let loaded: boolean = false;
 
 	let joinId: string;
-	let createId: string = generateRoomId();
+	let createId: string;
 	let createPin: string;
+
+	let showAdvancedCreateSettings: boolean = false;
 
 	onMount(() => {
 		// Redirect traffic from 404 page to corresponding room
@@ -24,48 +26,88 @@
 		return Math.floor(Math.random() * (max - min + 1) + min).toString();
 	}
 
-	function joinRoom() {
-		if (joinId) goto(base + '/' + joinId);
+	function generatePin(): string {
+		let length: number = 4;
+		let chars: string = '0123456789';
+		return [...Array(length)].map((i) => chars[Math.floor(Math.random() * chars.length)]).join('');
 	}
 
-	function createRoom() {}
+	function joinRoom() {
+		if (joinId) goto(base + '/room/' + joinId);
+	}
+
+	function openController() {
+		if (joinId) goto(base + '/control/' + joinId);
+	}
+
+	function quickCreateRoom() {
+		createId = generateRoomId();
+		createPin = generatePin();
+		createRoom();
+	}
+
+	function createRoom() {
+		if (createId && createPin) {
+			goto(base + '/control/' + createId + '?pin=' + createPin);
+		}
+	}
 </script>
 
 <div id="main" class="w-screen h-screen flex justify-center">
 	{#if loaded}
 		<div class="flex justify-center p-5 lg:p-10 h-full w-full lg:w-1/2">
 			<div class="flex flex-col justify-between h-full w-full">
+				<!-- Top Content -->
 				<div class="flex flex-col items-center w-full">
 					<div class="flex flex-col items-center w-full">
+						<!-- Title -->
 						<div class="flex flex-col items-center mb-5 lg:mb-10">
 							<h1 class="text-white text-3xl md:text-4xl lg:text-5xl mb-2">DLaxV Shot Clock</h1>
 							<h3 class="text-white text-md lg:text-xl text-center">
 								Free & Open Source online shot clock application
 							</h3>
 						</div>
+
+						<!-- Join Room -->
 						<div class="w-full lg:w-7/12 mb-10 lg:mb-20">
 							<div class="mb-3">
 								<h4 class="text-white text-md uppercase">Join room</h4>
 							</div>
-							<div class="grid grid-cols-inputs-2 mb-3">
+							<div class="grid grid-cols-inputs-3-button mb-3">
 								<span class="uppercase text-white text-xs font-bold">Room ID</span>
 								<div />
+								<div />
 								<input class="px-3 py-2 rounded" bind:value={joinId} />
-								<button class="btn" on:click={joinRoom}>Join</button>
+								<button class="btn text-xl ml-2" on:click={joinRoom}>Join Room</button>
+								<button class="btn text-xs ml-2" on:click={openController}
+									>Open<br />Controller</button
+								>
 							</div>
 						</div>
+
+						<!-- Create Room -->
 						<div class="w-full lg:w-7/12">
 							<div class="mb-3">
 								<h4 class="text-white text-md uppercase">Create room</h4>
 							</div>
-							<div class="grid grid-cols-inputs-3 mb-3">
-								<span class="uppercase text-white text-xs font-bold">Room ID</span>
-								<span class="uppercase text-white text-xs font-bold">Control PIN</span>
-								<div />
-								<input class="px-3 py-2 mr-2 rounded" bind:value={createId} />
-								<input class="px-3 py-2 rounded" bind:value={createPin} />
-								<button class="btn" on:click={createRoom}>Create</button>
+							<div class="flex mb-3">
+								<button class="btn text-lg" on:click={quickCreateRoom}>Quick create</button>
+								<button
+									class="btn text-lg ml-2"
+									on:click={() => (showAdvancedCreateSettings = !showAdvancedCreateSettings)}
+									>Custom settings</button
+								>
 							</div>
+							{#if showAdvancedCreateSettings}
+								<div class="grid grid-cols-inputs-3 mb-3">
+									<span class="uppercase text-white text-xs font-bold">Room ID</span>
+									<span class="uppercase text-white text-xs font-bold">Control PIN</span>
+									<div />
+									<input class="px-3 py-2 mr-2 rounded" bind:value={createId} />
+									<input class="px-3 py-2 rounded" bind:value={createPin} />
+									<button class="btn text-xs ml-2" on:click={createRoom}>Create<br />Custom</button>
+								</div>
+							{/if}
 						</div>
 					</div>
 				</div>
@@ -103,6 +145,6 @@
 	}
 
 	.btn {
-		@apply p-2 mx-2 border rounded text-white text-xl;
+		@apply px-2 py-1 border rounded text-white;
 	}
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         'inputs-2': 'minmax(0, 1fr) auto',
+        'inputs-3-button': 'minmax(0, 1fr) repeat(2, auto)',
         'inputs-3': 'repeat(2, minmax(0, 1fr)) auto'
       }
     },
