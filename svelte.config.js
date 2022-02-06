e81d2b4e87705f891f6ac8caf7512dee6c2c1cd9
tobire <mail@tobias-reinke.de>
2022-02-06 17:58:33 +0100

    Updated path for production deployment

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     paths: {
-      base: dev ? '' : '/shotclock-web-client'
+      base: dev ? '' : ''
     }
   }
 };
