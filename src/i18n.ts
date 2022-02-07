5a088b680f8cf1b2c9291645a462562240575b7c
tobire <mail@tobias-reinke.de>
2022-02-07 10:00:50 +0100

    Set correct language on load

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,4 @@
-import { register, init } from 'svelte-i18n';
+import { register, init, getLocaleFromNavigator, getLocaleFromQueryString } from 'svelte-i18n';
 import { browser } from '$app/env';
 
 register('en', () => import('./locales/en.json'));
@@ -11,7 +11,10 @@ if (browser) {
 	// don't put this inside `load`, otherwise it will gets executed every time you changed route on client side
 	init({
 		fallbackLocale: defaultLocale,
-		initialLocale: defaultLocale
+		initialLocale:
+			getLocaleFromQueryString('lang') != null
+				? getLocaleFromQueryString('lang')
+				: getLocaleFromNavigator()
 	});
 }
 
@@ -20,7 +23,7 @@ export const load = ({ page }) => {
 		// init on server side only, need to get query from `page.query.get("lang")`
 		init({
 			fallbackLocale: defaultLocale,
-			initialLocale: defaultLocale
+			initialLocale: page.query.get('lang')
 		});
 	}
 };
